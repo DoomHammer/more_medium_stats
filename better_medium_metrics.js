d6b8cd27ddc8a44ffffa1173e39dfc574debd80e
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-01-02 17:07:34 +0100

    Apply some FP :D

--- better_medium_metrics.js
+++ better_medium_metrics.js
@@ -1,38 +1,128 @@
 "use strict";
 
-function appendColumn(row, number, number_round, text)
+const id = x => x;
+
+const metrics = [
+    {
+        name: "Fan ratio",
+        fn: _ => (_.fans / _.reads) * 100,
+        display_fn: x => `${Math.round(x)}%`,
+        description: "The fan rate is obtained by dividing the number of people who have clapped (called Fans) by the number of reads. For me this is a reliable measure of actual engagement. Views, reads and claps are nothing but vanity metrics alone. The Fan Rate is tracking how many people who presumably read the article actually cared to clap for it",
+    },
+    {
+        name: "Claps",
+        fn: _ => _.numberOfClaps,
+        display_fn: id,
+        description: "Number of claps",
+    },
+    {
+        name: "Claps per fan",
+        fn: _ => _.numberOfClaps / _.fans,
+        display_fn: Math.round,
+        description: " Audiences on Medium mostly clap one time like a like, but they can choose to applaud an article up to 50 times, called a standing ovation. While this clap feature is very hackable, I still think it can be relevant if the sample size of views and fans is large enough, and Medium’s algorithm certainly understands it rather well. As such, it’s helpful as a writer to keep track of it. A general baseline is for an article to average more than 10 claps per fan. While the majority of people might only clap once, you want enough people to clap over 20 times to counter-balance this",
+    },
+    {
+        name: "Standing ovations",
+        fn: _ => _.standingOvations,
+        display_fn: id,
+        description: "50 claps from a single fan",
+    },
+    {
+        name: "Standing ovations per fan",
+        fn: _ => _.standingOvations / _.fans,
+        display_fn: Math.round,
+        description: "So assuming your article isn’t an article boosted by fake traffic (i.e. an airdrop), getting standing ovations (that consist of 50 claps) will be relatively rare. However if an article truly resonates with people, you will notice multiple standing ovations. By simply counting them, and dividing that by the number of total people who clapped (fans), you can get a reliable impact on the high-octane merits of your article",
+    },
+    {
+        name: "Comments Per 1000 Views",
+        fn: _ => _.numberOfComments / (_.views * 1000),
+        display_fn: Math.round,
+        description: "It’s fine if people read or clap for your work, but real engagement is not a signal. But actual highlights (points they agree with or find most salient) and comments are. They signal that your audience was emotionally engaged in the topic, regardless of the slant of the comment. We are people, real interaction does matter. If an article does not receive any comments it’s not emotionally impactful, authentic or does not present the material in a relatable manner. In practice counting highlights doesn’t make much sense, but tabulating the number of comments per 1,000 views makes sense",
+    },
+];
+
+async function main()
 {
-    const spanValue = document.createElement("span");
-    spanValue.setAttribute('class', 'sortableTable-value');
-    spanValue.appendChild(document.createTextNode(number));
+    const header_row_element = document.getElementsByClassName("sortableTableHeaders")[0].getElementsByTagName("tr")[0];
+    const addHeader = appendHeaders(header_row_element)
 
-    const spanNumber = document.createElement("span");
-    spanNumber.setAttribute('class', 'sortableTable-number');
-    spanNumber.appendChild(document.createTextNode(number_round));
+    metrics.map(metric => metric.name).forEach(addHeader);
 
-    const spanShow = document.createElement("span");
-    spanShow.setAttribute('class', 'u-sm-show');
-    spanShow.appendChild(document.createElement('br'));
-    spanShow.appendChild(document.createTextNode(text));
-    spanNumber.appendChild(spanShow);
+    const table = document.getElementsByClassName("js-statsTableBody")[0];
+    const rows = table.getElementsByClassName("js-statsTableRow");
 
-    const cell = document.createElement("td");
-    cell.appendChild(spanValue);
-    cell.appendChild(spanNumber);
+    Array.from(rows).forEach(row =>
+    {
+        const addColumn = addColumnToRow(row);
+        const article_data = await getArticleData(row);
 
-    row.appendChild(cell);
+        metrics.forEach(metric => addColumn(metric.fn(article_data), metric.display_fn(metric.fn(article_data)), metric.name));
+    });
+};
+
+async function getArticleData(row) 
+{
+    const tds = row.getElementsByTagName("td");
+    const postId = row.getAttribute("data-action-value");
+
+    const views = tds[1].getElementsByClassName("sortableTable-number")[0].getAttribute("title").replace(/,/g, "");
+    const reads = tds[2].getElementsByClassName("sortableTable-number")[0].getAttribute("title").replace(/,/g, "");
+    const fans = tds[4].getElementsByClassName("sortableTable-number")[0].getAttribute("title");
+
+    getClaps(row, postId, fans);
+    getResponses(row, postId, views);
+
+    return {
+        numberOfComments,
+        numberOfClaps,
+        standingOvations,
+        // trochę się gubię w tym kodzie, natomiast jesli ta funkcja zwróci 3 powyższe liczby to wszystko powinno działać
+        // te rzeczy niżej już są zwracane
+        fans,
+        reads,
+        views,
+    }
 }
 
-function appendHeaders(row, text)
+function addColumnToRow(row)
 {
-    const buttonText = document.createElement("button");
-    buttonText.appendChild(document.createTextNode(text));
+    return function(number, number_round, text)
+    {
+        const spanValue = document.createElement("span");
+        spanValue.setAttribute('class', 'sortableTable-value');
+        spanValue.appendChild(document.createTextNode(number));
 
-    const cell = document.createElement("th");
-    cell.setAttribute('class', 'sortableTable-header');
-    cell.appendChild(buttonText);
+        const spanNumber = document.createElement("span");
+        spanNumber.setAttribute('class', 'sortableTable-number');
+        spanNumber.appendChild(document.createTextNode(number_round));
 
-    row.appendChild(cell);
+        const spanShow = document.createElement("span");
+        spanShow.setAttribute('class', 'u-sm-show');
+        spanShow.appendChild(document.createElement('br'));
+        spanShow.appendChild(document.createTextNode(text));
+        spanNumber.appendChild(spanShow);
+
+        const cell = document.createElement("td");
+        cell.appendChild(spanValue);
+        cell.appendChild(spanNumber);
+
+        row.appendChild(cell);
+    };
+}
+
+function appendHeaders(row)
+{
+    return function(text)
+    {
+        const buttonText = document.createElement("button");
+        buttonText.appendChild(document.createTextNode(text));
+
+        const cell = document.createElement("th");
+        cell.setAttribute('class', 'sortableTable-header');
+        cell.appendChild(buttonText);
+
+        row.appendChild(cell);
+    }
 }
 
 function stripMediumJson(text) {
@@ -143,55 +233,4 @@ function getResponses(row, postId, views) {
   processResponses();
 }
 
-function makeGatherArticleData(row, postId, views, fans) {
-  return function() {
-    getClaps(row, postId, fans);
-    getResponses(row, postId, views);
-  };
-}
-
-const header_row_element = document.getElementsByClassName('sortableTableHeaders')[0].getElementsByTagName('tr')[0];
-
-["Fan ratio", "Claps", "Claps per fan", "Standing ovations", "Standing ovations per fan", "Comments per 1000 views"]
-    .forEach(x => appendHeaders(header_row_element, x))
-
-Array.from(document.getElementsByClassName('js-statsTableBody')).forEach(
-  item => {
-    var rows = item.getElementsByClassName('js-statsTableRow');
-    Array.from(rows).forEach(
-      function(row) {
-        var tds = row.getElementsByTagName("td");
-        var postId = row.getAttribute('data-action-value');
-        var views = tds[1].getElementsByClassName('sortableTable-number')[0].getAttribute('title').replace(/,/g, '');
-        var read = tds[2].getElementsByClassName('sortableTable-number')[0].getAttribute('title').replace(/,/g, '');
-        var fans = tds[4].getElementsByClassName('sortableTable-number')[0].getAttribute('title');
-        /*
-        2- Fan Rate
-        The fan rate is obtained by dividing the number of people who have clapped (called Fans) by the number of reads. For me this is a reliable measure of actual engagement. Views, reads and claps are nothing but vanity metrics alone.
-        The Fan Rate is tracking how many people who presumably read the article actually cared to clap for it.
-        */
-        var fan_ratio = fans / read * 100;
-        var fan_ratio_round = Math.round(fan_ratio);
-        appendColumn(row, fan_ratio, fan_ratio_round + '%', 'fan ratio');
-        /*
-        3- Comments Per 1000 Views
-        It’s fine if people read or clap for your work, but real engagement is not a signal. But actual highlights (points they agree with or find most salient) and comments are. They signal that your audience was emotionally engaged in the topic, regardless of the slant of the comment. We are people, real interaction does matter.
-        If an article does not receive any comments it’s not emotionally impactful, authentic or does not present the material in a relatable manner.
-        In practice counting highlights doesn’t make much sense, but tabulating the number of comments per 1,000 views makes sense.
-        */
-        let gatherArticleData = makeGatherArticleData(row, postId, views, fans);
-        gatherArticleData();
-        /*
-        4- Average Claps per Fan
-        Audiences on Medium mostly clap one time like a like, but they can choose to applaud an article up to 50 times, called a standing ovation. While this clap feature is very hackable, I still think it can be relevant if the sample size of views and fans is large enough, and Medium’s algorithm certainly understands it rather well. As such, it’s helpful as a writer to keep track of it.
-        A general baseline is for an article to average more than 10 claps per fan. While the majority of people might only clap once, you want enough people to clap over 20 times to counter-balance this.
-        */
-
-        /*
-        5- Standing Ovations to Fans Ratio
-        So assuming your article isn’t an article boosted by fake traffic (i.e. an airdrop), getting standing ovations (that consist of 50 claps) will be relatively rare. However if an article truly resonates with people, you will notice multiple standing ovations. By simply counting them, and dividing that by the number of total people who clapped (fans), you can get a reliable impact on the high-octane merits of your article.
-        */
-      }
-    );
-  }
-);
+main();
